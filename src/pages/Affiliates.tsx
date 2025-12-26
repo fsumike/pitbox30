cd0b5f477a2deaa79d626ce9316a6c098fedfcce
fsumike <mg91648@yahoo.com>
2025-12-26 15:20:09 -0800

    Updated Affiliates.tsx

--- src/pages/Affiliates.tsx
+++ src/pages/Affiliates.tsx
@@ -273,9 +273,9 @@ function Affiliates() {
                 </div>
               </div>
 
-              <div className="mb-4">
+              <div className="mb-4 bg-gray-900 rounded-lg p-4">
                 <img
-                  src="/logo.png"
+                  src="https://cdn.myracepass.com/v1/siteresources/35514/v1/img/logo.png"
                   alt="Silver Dollar Speedway"
                   className="w-full h-auto object-contain max-h-32"
                 />
@@ -313,9 +313,9 @@ function Affiliates() {
                 </div>
               </div>
 
-              <div className="mb-4">
+              <div className="mb-4 bg-gray-900 rounded-lg p-4">
                 <img
-                  src="/image (2).png"
+                  src="https://www.thunderbowlraceway.com/wp-content/uploads/2020/01/Logo.png"
                   alt="Thunderbowl Raceway"
                   className="w-full h-auto object-contain max-h-32"
                 />
