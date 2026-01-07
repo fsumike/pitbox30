3129d71730efff5eaf073c68c525e56dd5eca874
fsumike <mg91648@yahoo.com>
2026-01-06 16:08:40 -0800

    Updated capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.pitbox.app',
-  appName: 'PIT-BOX',
+  appName: 'Pit-Box',
   webDir: 'dist',
   server: {
     androidScheme: 'https',
