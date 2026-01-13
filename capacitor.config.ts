00f6429c07b226830f4d225c3377de47342fb39f
fsumike <mg91648@yahoo.com>
2026-01-12 23:27:55 -0800

    Updated capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.pitbox.racing',
+  appId: 'com.pitbox.app',
   appName: 'PitBox',
   webDir: 'dist',
   server: {
